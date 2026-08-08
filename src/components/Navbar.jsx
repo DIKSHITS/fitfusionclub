5e4f3c60b83a72d7721a71236161951673ba0193
Dikshit Raj <dikshitraj@Dikshits-MacBook-Air.local>
2026-08-09 00:31:33 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,58 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../assets/images/logo.png";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <header className="navbar">
 
       <div className="container">
 
         {/* Logo */}
+        <a href="#home" className="logo" onClick={closeMenu}>
+          <img
+            className="logo-image"
+            src={logo}
+            alt="Fusion Fitness Wellness Centre"
+          />
+        </a>
 
-        <div className="logo">
-          <img className="logo-image" src={logo} alt="Fusion Fitness Wellness Centre" />
-
-        </div>
+        {/* Mobile Menu Button */}
+        <button
+          className={`menu-toggle ${menuOpen ? "open" : ""}`}
+          onClick={() => setMenuOpen(!menuOpen)}
+          aria-label="Toggle navigation menu"
+          aria-expanded={menuOpen}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
 
         {/* Navigation */}
-
-        <nav>
+        <nav className={menuOpen ? "nav-open" : ""}>
 
           <ul className="nav-links">
 
             <li>
-              <a href="#home" className="active">
+              <a
+                href="#home"
+                className="active"
+                onClick={closeMenu}
+              >
                 Home
               </a>
             </li>
 
             <li>
-              <a href="#about">
+              <a
+                href="#about"
+                onClick={closeMenu}
+              >
                 About Us
               </a>
             </li>
 
             <li>
-              <a href="#services">
+              <a
+                href="#services"
+                onClick={closeMenu}
+              >
                 Services
               </a>
             </li>
 
             <li>
-              <a href="#programs">
+              <a
+                href="#programs"
+                onClick={closeMenu}
+              >
                 Programs
               </a>
             </li>
 
             <li>
-              <a href="#blog">
+              <a
+                href="#blog"
+                onClick={closeMenu}
+              >
                 Blog
               </a>
             </li>
 
             <li>
-              <a href="#contact">
+              <a
+                href="#contact"
+                onClick={closeMenu}
+              >
                 Contact Us
               </a>
             </li>
@@ -61,11 +99,14 @@ const Navbar = () => {
 
         </nav>
 
-        {/* Button */}
-
-        <button className="consult-btn">
+        {/* Consultation Button */}
+        <a
+          href="#contact"
+          className="consult-btn"
+          onClick={closeMenu}
+        >
           Book Consultation
-        </button>
+        </a>
 
       </div>
 

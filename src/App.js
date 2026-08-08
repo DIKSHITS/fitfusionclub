ed98168a9f1f2d7c5948d7e93bfddb7ddc6a7807
Dikshit Raj <dikshitraj@Dikshits-MacBook-Air.local>
2026-08-09 00:19:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import "./App.css";
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
@@ -6,6 +8,7 @@ import Programs from "./components/Programs";
 import Testimonials from "./components/Testimonials";
 import Blog from "./components/Blog";
 import Footer from "./components/Footer";
+import { FaWhatsapp } from "react-icons/fa";
 
 function App() {
   return (
@@ -18,6 +21,16 @@ function App() {
       <Testimonials />
       <Blog />
       <Footer />
+
+      <a
+        className="whatsapp-float"
+        href="https://wa.me/919007089970"
+        target="_blank"
+        rel="noreferrer"
+        aria-label="Chat on WhatsApp"
+      >
+        <FaWhatsapp />
+      </a>
     </>
   );
 }

d628b7c9ccf9952d5d522e8efa8a4321ac549577
Dikshit Raj <dikshitraj@Dikshits-MacBook-Air.local>
2026-08-26 15:49:35 +0530

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Hero.css";
 
 import heroImage from "../assets/images/hero.WebP";
-import heroMobileImage from "../assets/images/hero-mobile.WebP";
+import heroMobileImage from "../assets/images/hero-mobile.webp";
 
 function Hero() {
   return (
